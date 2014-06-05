28af0fe9ebd05eb755cc193901486066face3162
Alina Saalfeld <alina.saalfeld@ymail.com>
2014-06-05 16:23:08 +0200

    Automatisches Reconnecten, falls zu lange kein Ping kam

--- Messager.rb
+++ Messager.rb
@@ -9,26 +9,24 @@ class Messager
 		@stop = false
 		@run = true
 		@write_mutex = Mutex.new
+		@ping_time = Time.new
 
-
+		@host = host
+		@port = port
+		@oauth = oauth
 		@channel = channel
 		@username = username
 		@logger = logger
 
-		@write_mutex.synchronize do
-			@irc = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
-			@irc.connect(Socket.pack_sockaddr_in(port, host))
-			@irc.puts("PASS " + oauth)
-			@irc.puts("NICK " + @username)
-			@irc.puts("TWITCHCLIENT 1")
-			@irc.write("JOIN " + @channel + "\n")
-		end
+		connect()
 
 
 		Thread.new do
 			while @run
 				element = @queue.pop(true) rescue nil
-				if element != nil
+				if @ping_time + 300 < Time.new()
+					connect()
+				elsif element != nil
 					@write_mutex.synchronize do
 						@irc.puts(element)
 					end
@@ -42,11 +40,29 @@ class Messager
 		end
 	end
 
+	def connect()
+		@write_mutex.synchronize do
+			if !closed?
+				@irc.close()
+			end
+			@irc = Socket.new(Socket::AF_INET, Socket::SOCK_STREAM)
+			@irc.connect(Socket.pack_sockaddr_in(@port, @host))
+			@irc.puts("PASS " + @oauth)
+			@irc.puts("NICK " + @username)
+			@irc.puts("TWITCHCLIENT 1")
+			@irc.write("JOIN " + @channel + "\n")
+		end
+	end
+
 	def stop()
 		@stop = true
 		while @run
 			sleep(2)
 		end
+	end
+
+	def ping()
+		@ping_time = Time.new
 	end
 
 	def message(msg)
@@ -61,11 +77,19 @@ class Messager
 	end
 
 	def gets()
-		return @irc.gets()
+		if !closed?
+			return @irc.gets()
+		else
+			return nil
+		end
 	end
 
 	def closed?()
-		return @irc.closed?()
+		if @irc != nil
+			return @irc.closed?()
+		else
+			return true
+		end
 	end
 
 end
